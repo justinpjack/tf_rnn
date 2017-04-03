7acc621869b3cfd1e277c23bebd7f3bc9bb7b552
justinpjack <justinpjack@gmail.com>
2017-04-02 18:23:39 -0700

    "update"

--- ptb_word_lm.py
+++ ptb_word_lm.py
@@ -108,13 +108,13 @@ class PTBModel(object):
     # Slightly better results can be obtained with forget gate biases
     # initialized to 1 but the hyperparameters of the model would need to be
     # different than reported in the paper.
-    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(
-        size, forget_bias=0.0, state_is_tuple=True) # justin added - was tf.contrib.rnn.BasicLSTMCell
+    lstm_cell =  tf.contrib.rnn.BasicLSTMCell(
+        size, forget_bias=0.0, state_is_tuple=True) # justin added - was tf.nn.rnn_cell.BasicLSTMCell
     if is_training and config.keep_prob < 1:
       lstm_cell = tf.contrib.rnn.DropoutWrapper(
           lstm_cell, output_keep_prob=config.keep_prob)
-    cell = tf.nn.rnn_cell.MultiRNNCell(
-        [lstm_cell] * config.num_layers, state_is_tuple=True) # justin added - was tf.contrib.rnn.MultiRNNCell
+    cell = tf.contrib.rnn.MultiRNNCell(
+        [lstm_cell] * config.num_layers, state_is_tuple=True) # justin added - was tf.nn.rnn_cell.MultiRNNCell
 
     self._initial_state = cell.zero_state(batch_size, data_type())
 
